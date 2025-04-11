4c239d142a7f9f0888253226050f189e5f16b4fe
gregorygustavo80 <gregorygustavo@hotmail.com>
2025-04-10 21:32:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,9 @@ def comentar():
     return jsonify({"mensagem": "Comentário enviado com sucesso!"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
