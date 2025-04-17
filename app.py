da2585297ff55d176bb27432f2880de9873ea62c
gregorygustavo80 <gregorygustavo@hotmail.com>
2025-04-17 14:01:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ def assinar():
     return redirect(url_for('index'))  # Redireciona após salvar a assinatura
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
